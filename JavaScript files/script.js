91a2914cadc5e26615163f09ab6c28cfa9afd53f
Alexander Corrigan <rednaxc1@gmail.com>
2025-05-09 11:24:44 -0400

    Commit 4, more content added, and adjusted.

--- JavaScript files/script.js
+++ JavaScript files/script.js
@@ -1,1 +1,8 @@
 // hi
+// Toggle the mobile navigation menu when the button is clicked
+const menuToggle = document.getElementById('menu-toggle');
+const mobileNav = document.getElementById('mobile-nav');
+
+menuToggle.addEventListener('click', () => {
+  mobileNav.classList.toggle('show');
+});
